c0f5233ff55093d7ba644f00711a060be7fffa17
Naveen Kaje <nkaje@codeaurora.org>
2013-10-02 21:11:04 +0000

    msm_serial_hs: fix unbalanced clock vote during port close
    This change fixes the clock voting logic when the port is closed and
    prevents the clock votes from going negative. Clocks are voted for when
    the port is being closed and the conditional unvote based on the clock
    state will always be true if the earlier vote is not downvoted before
    this check is done. This change also protects agains erroneous, unbalanced
    client votes, leading to instabilities.
    
    CRs-Fixed: 550668
    Change-Id: I391ac69601314757821b3ffb931a00647a64c583
    Signed-off-by: Naveen Kaje <nkaje@codeaurora.org>

--- drivers/tty/serial/msm_serial_hs_lge.c
+++ drivers/tty/serial/msm_serial_hs_lge.c
@@ -434,7 +434,17 @@ static int msm_hs_clock_vote(struct msm_hs_port *msm_uport)
 
 static void msm_hs_clock_unvote(struct msm_hs_port *msm_uport)
 {
-	if (1 == __atomic_add_unless(&msm_uport->clk_count, -1, 0)) {
+	int rc = atomic_read(&msm_uport->clk_count);
+
+	if (rc <= 0) {
+		WARN(rc, "msm_uport->clk_count < 0!");
+		dev_err(msm_uport->uport.dev,
+			"%s: Clocks count invalid  [%d]\n", __func__, rc);
+		return;
+	}
+
+	rc = atomic_dec_return(&msm_uport->clk_count);
+	if (0 == rc) {
 		/* Turn off the core clk and iface clk*/
 		clk_disable_unprepare(msm_uport->clk);
 		if (msm_uport->pclk)
@@ -3730,7 +3740,6 @@ static void msm_hs_shutdown(struct uart_port *uport)
 	mb();
 
 	msm_hs_clock_unvote(msm_uport);
-
 	if (msm_uport->clk_state != MSM_HS_CLK_OFF) {
 		/* to balance clk_state */
 		msm_hs_clock_unvote(msm_uport);
